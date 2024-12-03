f9e7ad09c4207440040b2016d98cf2eede451f2f
Ashish Ajin Thomas <Coolioboss7@gmail.com>
2024-12-03 17:17:08 -0500

    Add JavaDocs & Fix Checkstyle Errors

--- src/main/java/interface_adapter/delete_event/DeleteEventController.java
+++ src/main/java/interface_adapter/delete_event/DeleteEventController.java
@@ -4,15 +4,23 @@ import entity.Event;
 import use_case.delete_event.DeleteEventInputBoundary;
 import use_case.delete_event.DeleteEventInputData;
 
+/**
+ * The controller for Delete Event.
+ */
 public class DeleteEventController {
-  private final DeleteEventInputBoundary deleteEventUseCaseInteractor;
+    private final DeleteEventInputBoundary deleteEventUseCaseInteractor;
 
-  public DeleteEventController(DeleteEventInputBoundary deleteEventUseCaseInteractor) {
-    this.deleteEventUseCaseInteractor = deleteEventUseCaseInteractor;
-  }
+    public DeleteEventController(DeleteEventInputBoundary deleteEventUseCaseInteractor) {
+        this.deleteEventUseCaseInteractor = deleteEventUseCaseInteractor;
+    }
 
-  public void execute(Event event) {
-    DeleteEventInputData deleteEventInputData = new DeleteEventInputData(event);
-    deleteEventUseCaseInteractor.execute(deleteEventInputData);
-  }
+    /**
+     * Executes the change calendar day use case with the given calendars and date.
+     *
+     * @param event Event to be Deleted
+     */
+    public void execute(Event event) {
+        final DeleteEventInputData deleteEventInputData = new DeleteEventInputData(event);
+        deleteEventUseCaseInteractor.execute(deleteEventInputData);
+    }
 }
--- src/main/java/interface_adapter/delete_event/DeleteEventPresenter.java
+++ src/main/java/interface_adapter/delete_event/DeleteEventPresenter.java
@@ -4,35 +4,38 @@ import interface_adapter.change_calendar_day.ChangeCalendarDayViewModel;
 import use_case.delete_event.DeleteEventOutputBoundary;
 import use_case.delete_event.DeleteEventOutputData;
 
+/**
+ * The presenter for Change Month Calendar Use Case.
+ */
 public class DeleteEventPresenter implements DeleteEventOutputBoundary {
-  private final DeleteEventViewModel deleteEventViewModel;
-  private final ChangeCalendarDayViewModel dayViewModel;
+    private final DeleteEventViewModel deleteEventViewModel;
+    private final ChangeCalendarDayViewModel dayViewModel;
 
-  public DeleteEventPresenter(DeleteEventViewModel deleteEventViewModel,
-                              ChangeCalendarDayViewModel dayViewModel) {
-    this.deleteEventViewModel = deleteEventViewModel;
-    this.dayViewModel = dayViewModel;
-  }
+    public DeleteEventPresenter(DeleteEventViewModel deleteEventViewModel,
+                                ChangeCalendarDayViewModel dayViewModel) {
+        this.deleteEventViewModel = deleteEventViewModel;
+        this.dayViewModel = dayViewModel;
+    }
 
-  @Override
-  public void prepareSuccessView(DeleteEventOutputData outputData) {
-    // Update both view models
-    dayViewModel.removeEvent(outputData.getEvent());
-    dayViewModel.firePropertyChanged();
+    @Override
+    public void prepareSuccessView(DeleteEventOutputData outputData) {
+        // Update both view models
+        dayViewModel.removeEvent(outputData.getEvent());
+        dayViewModel.firePropertyChanged();
 
-    DeleteEventState state = new DeleteEventState();
-    state.setSelectedEvent(null);
-    state.setUseCaseFailed(false);
-    deleteEventViewModel.setState(state);
-    deleteEventViewModel.firePropertyChanged();
-  }
+        final DeleteEventState state = new DeleteEventState();
+        state.setSelectedEvent(null);
+        state.setUseCaseFailed(false);
+        deleteEventViewModel.setState(state);
+        deleteEventViewModel.firePropertyChanged();
+    }
 
-  @Override
-  public void prepareFailView(String error) {
-    DeleteEventState state = new DeleteEventState();
-    state.setError(error);
-    state.setUseCaseFailed(true);
-    deleteEventViewModel.setState(state);
-    deleteEventViewModel.firePropertyChanged();
-  }
+    @Override
+    public void prepareFailView(String error) {
+        final DeleteEventState state = new DeleteEventState();
+        state.setError(error);
+        state.setUseCaseFailed(true);
+        deleteEventViewModel.setState(state);
+        deleteEventViewModel.firePropertyChanged();
+    }
 }
