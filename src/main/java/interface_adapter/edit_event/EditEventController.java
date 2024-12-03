45716a338c389df07df09417464f6d5e8b7d95ad
Ashish Ajin Thomas <Coolioboss7@gmail.com>
2024-12-03 17:29:06 -0500

    Added JavaDocs for Edit Event Use Case, Fix Checkstyle Errors

--- src/main/java/interface_adapter/edit_event/EditEventController.java
+++ src/main/java/interface_adapter/edit_event/EditEventController.java
@@ -1,23 +1,36 @@
 package interface_adapter.edit_event;
 
-import use_case.edit_event.EditEventInputBoundary;
-import use_case.edit_event.EditEventInputData;
 import entity.Calendar;
 import entity.Event;
+import use_case.edit_event.EditEventInputBoundary;
+import use_case.edit_event.EditEventInputData;
 
+/**
+ * The controller for Edit Event.
+ */
 public class EditEventController {
-  private final EditEventInputBoundary editEventUseCaseInteractor;
+    private final EditEventInputBoundary editEventUseCaseInteractor;
 
-  public EditEventController(EditEventInputBoundary editEventUseCaseInteractor) {
-    this.editEventUseCaseInteractor = editEventUseCaseInteractor;
-  }
+    public EditEventController(EditEventInputBoundary editEventUseCaseInteractor) {
+        this.editEventUseCaseInteractor = editEventUseCaseInteractor;
+    }
 
-  public void execute(String eventName, String date, String startTime, String endTime,
-                      Calendar calendar, Event originalEvent) {
-    EditEventInputData editEventInputData = new EditEventInputData(
-      eventName, date, startTime, endTime, calendar, originalEvent
-    );
+    /**
+     * Executes the change calendar day use case with the given calendars and date.
+     *
+     * @param eventName Name of the Event
+     * @param date date of the event
+     * @param startTime start Time of the event
+     * @param endTime End TIme of the Event
+     * @param calendar Calendar
+     * @param originalEvent THe Original Event to Be Edited.
+     */
+    public void execute(String eventName, String date, String startTime, String endTime,
+                        Calendar calendar, Event originalEvent) {
+        final EditEventInputData editEventInputData = new EditEventInputData(
+                eventName, date, startTime, endTime, calendar, originalEvent
+        );
 
-    editEventUseCaseInteractor.execute(editEventInputData);
-  }
+        editEventUseCaseInteractor.execute(editEventInputData);
+    }
 }
--- src/main/java/interface_adapter/edit_event/EditEventPresenter.java
+++ src/main/java/interface_adapter/edit_event/EditEventPresenter.java
@@ -4,48 +4,55 @@ import interface_adapter.change_calendar_day.ChangeCalendarDayViewModel;
 import use_case.edit_event.EditEventOutputBoundary;
 import use_case.edit_event.EditEventOutputData;
 
+/**
+ * The presenter for Editing Event.
+ */
 public class EditEventPresenter implements EditEventOutputBoundary {
-  private final EditEventViewModel editEventViewModel;
-  private final ChangeCalendarDayViewModel dayViewModel;
+    private final EditEventViewModel editEventViewModel;
+    private final ChangeCalendarDayViewModel dayViewModel;
 
-  public EditEventPresenter(EditEventViewModel editEventViewModel,
-                            ChangeCalendarDayViewModel dayViewModel) {
-    this.editEventViewModel = editEventViewModel;
-    this.dayViewModel = dayViewModel;
-  }
-
-  @Override
-  public void prepareSuccessView(EditEventOutputData outputData) {
-    // Update both view models
-    dayViewModel.removeEvent(outputData.getOldEvent());
-    dayViewModel.addEvent(outputData.getNewEvent());
-    dayViewModel.firePropertyChanged();
-
-    EditEventState state = new EditEventState();
-    state.setUseCaseFailed(false);
-    editEventViewModel.setState(state);
-    editEventViewModel.firePropertyChanged();
-  }
-
-  @Override
-  public void prepareFailView(String error) {
-    EditEventState state = new EditEventState();
-
-    // Determine which type of error it is and set accordingly
-    if (error.toLowerCase().contains("name")) {
-      state.setEventNameError(error);
-    } else if (error.toLowerCase().contains("date")) {
-      state.setDateError(error);
-    } else if (error.toLowerCase().contains("time")) {
-      state.setTimeError(error);
-    } else if (error.toLowerCase().contains("calendar")) {
-      state.setCalendarError(error);
-    } else {
-      state.setGeneralError(error); // Use generalError instead of error
+    public EditEventPresenter(EditEventViewModel editEventViewModel,
+                              ChangeCalendarDayViewModel dayViewModel) {
+        this.editEventViewModel = editEventViewModel;
+        this.dayViewModel = dayViewModel;
     }
 
-    state.setUseCaseFailed(true);
-    editEventViewModel.setState(state);
-    editEventViewModel.firePropertyChanged();
-  }
+    @Override
+    public void prepareSuccessView(EditEventOutputData outputData) {
+        // Update both view models
+        dayViewModel.removeEvent(outputData.getOldEvent());
+        dayViewModel.addEvent(outputData.getNewEvent());
+        dayViewModel.firePropertyChanged();
+
+        final EditEventState state = new EditEventState();
+        state.setUseCaseFailed(false);
+        editEventViewModel.setState(state);
+        editEventViewModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        final EditEventState state = new EditEventState();
+
+        // Determine which type of error it is and set accordingly
+        if (error.toLowerCase().contains("name")) {
+            state.setEventNameError(error);
+        }
+        else if (error.toLowerCase().contains("date")) {
+            state.setDateError(error);
+        }
+        else if (error.toLowerCase().contains("time")) {
+            state.setTimeError(error);
+        }
+        else if (error.toLowerCase().contains("calendar")) {
+            state.setCalendarError(error);
+        }
+        else {
+            state.setGeneralError(error);
+        }
+
+        state.setUseCaseFailed(true);
+        editEventViewModel.setState(state);
+        editEventViewModel.firePropertyChanged();
+    }
 }
